82ee0f9eaddde9c615e1bb474e12a6602c7464ed
tama <hokuto_method@yahoo.co.jp>
2018-03-11 06:53:47 +0900

    up

---
+++ hanoi.py
@@ -0,0 +1,14 @@
+def hanoi_f(n,a,b,c):
+    if n == 1:
+        print a+'->'+c
+    else:
+        hanoi_f(n-1,a,c,b)
+        hanoi_f(1,a,b,c)
+        hanoi_f(n-1,b,a,c)
+
+def hanoi(n):
+    hanoi_f(n,'a','b','c')
+
+
+if __name__ == '__main__':
+    hanoi(5)
---
+++ test.py
@@ -0,0 +1,40 @@
+#! /bin/env python
+# coding:utf-8
+
+from random import randrange as rand
+import matplotlib.pyplot as plt
+import networkx as nx
+import pydot
+
+G = {}
+N = 10
+
+def gen_graph():
+    global G,graph,pos
+    G[0] = []
+    for i in range(1,N):
+        j = 0
+        G[i] = []
+        while(j == 0):
+            for k in range(i):
+                if(rand(2) == 0):
+                    G[k].append(i)
+                    G[i].append(k)
+                    j += 1
+    graph = nx.Graph()
+    for i in G:
+        graph.add_node(i)
+    for i in G:
+        for j in G[i]:
+            graph.add_edge(i,j)
+    pos=nx.spring_layout(graph, k = 5.)
+
+def my_draw_graph():
+    nx.draw(graph,pos)
+    plt.show()
+
+if __name__ == '__main__':
+    gen_graph()
+    print pos
+    my_draw_graph()
+
