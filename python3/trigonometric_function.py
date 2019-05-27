08917cae7d26487e743e62915eb42075e090ae0e
tama <tama@ttk1.net>
2019-05-28 02:34:46 +0900

    修正

--- python3/trigonometric_function.py
+++ python3/trigonometric_function.py
@@ -35,55 +35,58 @@ def f(x):
 pi = 3.141592653589793
 
 
-def mp(p1, p2):
+def g(p1, p2):
     if (p1[0] + p2[0]) == 0:
         return [0.0, 1.0]
     p = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
-    d = math.sqrt(p[0] ** 2 + p[1] ** 2)
-    return [p[0] / d, p[1] / d]
+    k = math.sqrt(p[0] ** 2 + p[1] ** 2)
+    return [p[0] / k, p[1] / k]
 
 
-def f(x):
-    if x < 0 or 1 < x:
-        raise Exception('xは0以上1以下の範囲で指定してください.')
-    t1 = 1.0
-    t2 = 0.0
-    p1 = [-1.0, 0.0]
-    p2 = [1.0, 0.0]
-    while t1 - t2 > 0.000000000000001:
-        m = (t1 + t2) / 2
-        if m > x:
-            t1 = m
-            p1 = mp(p1, p2)
+def f(t):
+    t1 = 0
+    t2 = pi
+    p1 = [1.0, 0.0]
+    p2 = [-1.0, 0.0]
+    while t2 - t1 > 0.000000000000001:
+        t3 = (t1 + t2) / 2
+        if t3 < t:
+            t1 = t3
+            p1 = g(p1, p2)
         else:
-            t2 = m
-            p2 = mp(p1, p2)
+            t2 = t3
+            p2 = g(p1, p2)
     return p1
 
 
-def sin(x):
-    return f(x/pi)[1]
+def sin(t):
+    return f(t)[1]
 
 
-def cos(x):
-    return f(x/pi)[0]
+def cos(t):
+    return f(t)[0]
 
 
-def tan(x):
-    p = f(x/pi)
+def tan(t):
+    p = f(t)
     return p[1] / p[0]
 
 
 if __name__ == '__main__':
     random.seed(0)
-    loop = [random.uniform(0, pi) for _ in range(10 * 1000)]
+    loop = [random.uniform(0, pi) for _ in range(100 * 1000)]
     with Benchmarker(width=20) as bench:
-        @bench('hoge')
+        @bench('tama')
         def _(bm):
             for r in loop:
                 sin(r)
 
-        @bench('piyo')
+        @bench('numpy')
         def _(bm):
             for r in loop:
                 np.sin(r)
+
+        @bench('math')
+        def _(bm):
+            for r in loop:
+                math.sin(r)
