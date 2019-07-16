5fd1e3b634e7063508f09ec7ed42049668acf23b
tama <tama@ttk1.net>
2019-07-17 01:26:17 +0900

    パターン追加

--- bash/condition.sh
+++ bash/condition.sh
@@ -23,6 +23,10 @@ echo 'T && F'
 T && F
 echo $?
 
+echo 'F && F'
+F && F
+echo $?
+
 echo '! F && T'
 ! F && T
 echo $?
@@ -39,8 +43,51 @@ echo '! (T && F)'
 ! (T && F)
 echo $?
 
+
+echo 'T || T'
+T || T
+echo $?
+
+echo 'F || T'
+F || T
+echo $?
+
+echo 'T || F'
+T || F
+echo $?
+
+echo 'F || F'
+F || F
+echo $?
+
+echo '! F || T'
+! F || T
+echo $?
+
+echo '! T || F'
+! T || F
+echo $?
+
+echo '! (F || T)'
+! (F || T)
+echo $?
+
+echo '! (T || F)'
+! (T || F)
+echo $?
+
+
+echo 'T F'
+T F
+echo $?
+
+echo 'F T'
+F T
+echo $?
+
 exit 0
 
+# 結果
 cat << EOS
 $ ./condition.sh
 T && T
@@ -52,6 +99,9 @@ F
 1
 T && F
 T
+F
+1
+F && F
 F
 1
 ! F && T
@@ -68,4 +118,38 @@ F
 T
 F
 0
+T || T
+T
+0
+F || T
+F
+T
+0
+T || F
+T
+0
+F || F
+F
+F
+1
+! F || T
+F
+0
+! T || F
+T
+F
+1
+! (F || T)
+F
+T
+1
+! (T || F)
+T
+1
+T F
+T
+0
+F T
+F
+1
 EOS
